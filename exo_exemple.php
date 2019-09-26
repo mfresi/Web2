b2b941c4b7f397d2aece1be6cf723f43f4fd18e0
Mattei Fresi <mfresi@la-providence.net>
2019-09-26 11:51:31 +0200

    comit 1

---
+++ exo_exemple.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+    <link href="style.css" rel="steelsheet">
+    <title>Entrainement PHP</title>
+    </head>
+    <body>
+        <h1>
+        <?php echo "Serveur Fresi"; ?>
+        </h1>
+        <h2>
+            test 1
+        </h2>
+        <div>
+        <?php
+            $VariableTest = "Je suis une variable";
+            ?><div class="rouge"> <?php echo $VariableTest; 
+        ?>
+            </div>
+        </div>
+    </body>
+</html>
+
