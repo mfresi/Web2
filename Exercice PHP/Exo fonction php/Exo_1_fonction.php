29d0118fe890ea8dee7e4a10dd0b8e45950adca8
Mattei Fresi <mfresi@la-providence.net>
2019-11-21 11:19:10 +0100

    comit langlace

--- Exercice PHP/Exo fonction php/Exo_1_fonction.php
+++ Exercice PHP/Exo fonction php/Exo_1_fonction.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php Function tableau(){?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Exo_1_fonction.css">
     <title>Ecercice 1 fonction</title>
-    <h1> Exercice 1 fonction</h1>
+    <h1> Exercice_1_fonction</h1>
 </head>
-<body>
-   
-        
+<body>langlace
+<?php Function tableau(){?>
+
         <table border="1">
 <tr>
 <?php $tab[0] = array("nom"=> "Fresi","Prenom"=>"Mattei","motDePasse"=>"1234");
@@ -41,6 +40,8 @@
     <div class="tableaubas">
         <?php echo tableau(); ?>
     </div>
+
+    <a href="http://192.168.65.195/ProjetBTS/Mattei/Exercice%20PHP/Exo%20fonction%20php/">Retour</a><br>
 
     
 </body>
---
+++ page d'accueil/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styleExo.css">
+    <title>Page d'accueil</title>
+    <h2>Bienvenue sur ma page d'accueil</h2>
+</head>
+<body>
+
+<a href="http://192.168.65.195/ProjetBTS/Mattei/Exercice%20HTML/">Exercic HTML</a><br>
+<a href="http://192.168.65.195/ProjetBTS/Mattei/Exercice%20Css/">Exercic CSS</a><br>
+<a href="http://192.168.65.195/ProjetBTS/Mattei/Exercice%20PHP/">Exercic PHP</a><br>
+
+
+
+</body>
+</html>
